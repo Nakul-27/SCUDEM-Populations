26032dee609bbceb9a5b43f419dd1a8e1cded4fa
Nakul Rao <nakul_rao@nakuls-mbp.westpoint.edu>
2020-10-28 10:56:28 -0400

    Project Files

---
+++ src/models/Immigrant.h
@@ -0,0 +1,14 @@
+#ifndef IMMIGRANT_H
+#define IMMIGRANT_H value
+#include <string>
+using std::string;
+
+class Immigrant {
+ public:
+  std::string getMethod();
+
+ private:
+  std::string method;
+};
+#endif /* ifndef IMMIGRANT_H */
+
